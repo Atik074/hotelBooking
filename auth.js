e7cadae93c9c0c30a4558100f89740e327771320
atik074 <atiksmats@gmail.com>
2025-05-11 23:11:21 +0600

    social google login done by next auth

---
+++ auth.js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const {
+  auth,
+  signIn,
+  signOut,
+  handlers: { GET, POST },
+} = NextAuth({
+  providers: [
+   GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+    secret: process.env.AUTH_SECRET,
+ 
+});
